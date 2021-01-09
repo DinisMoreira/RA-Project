5cd3d2b95d2d38a37b621bd7506430686134c0cd
Dinis Moreira <up201503092@fe.up.pt>
2021-01-09 14:14:19 +0000

    Restructure

--- Edge.py
+++ Edge.py
@@ -1,14 +1,9 @@
-import random
-
 class Edge:
 
-    def __init__(self, node1, node2):
-        self.node1 = node1
-        self.node2 = node2
-        self.weight = random.uniform(0, 1)
+    def __init__(self, otherNode, weight):
+        self.otherNode = otherNode
+        self.weight = weight
 
     def print(self):
-        print("Node 1 = " + str(self.node1))
-        print("Node 2 = " + str(self.node2))
-        print("Weight = " + str(self.weight))
+        print("Connection to node " + str(self.otherNode) + " = " + str(self.weight))
         return
--- Graph.py
+++ Graph.py
@@ -1,4 +1,6 @@
 import random
+from Edge import Edge
+from Node import Node
 
 class Graph:
 
@@ -7,9 +9,22 @@ class Graph:
         self.edges = []
 
         for i in range(0, nodesNum):
-            self.nodes.append(i)
+            newNode = Node(i)
+            self.nodes.append(newNode)
+
+        #Generate Node Edges
+        for i in range(0, nodesNum):
+            for j in range(0, nodesNum):
+                if(i < j):
+                    newEdge = Edge(self.nodes[j],random.uniform(0, 1))
+                    self.nodes[i].connectedEdges.append(newEdge)
+                #elif(i > j):
+            
+            #generate new random weight for edges connected to nodes with bigger id numbers
+            #get the weight of the existing edge from the edges connected to nodes with lower id numbers
+
 
     def print(self):
-        print("Nodes = " + str(self.nodes))
-        print("Edges = " + str(self.edges))
+        #print("Nodes = " + str(self.nodes))
+        #print("Edges = " + str(self.edges))
         return
